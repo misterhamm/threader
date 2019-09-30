9bc674ebb9cf11595b9522722dfc7a52bca8ae1b
Chris Hamm <chamm@bizstream.com>
2019-09-29 21:06:48 -0400

    progress on making sure tweets to break words

--- app.js
+++ app.js
@@ -29,30 +29,87 @@ function liveUpdate() {
 }
 textArea.addEventListener('keyup', liveUpdate);
 
+
+// modStr.forEach(function(word, i){
+//   var wordLength = word.length + 1;
+//   testCharCount += wordLength;
+  
+//   if (testCharCount <= 280 && i != modStr.length - 1) {
+//     outputStr += word + ' ';
+//   }
+//   else if (testCharCount <= 280 && i == modStr.length - 1) {
+//     outputStr += word + ' ';
+//     console.log(outputStr );
+//     testCharCount = 0;
+//     outputStr = word + ' ';
+//   }
+//   else {
+//     console.log(outputStr );
+//     testCharCount = 0;
+//     outputStr = word + ' ';
+//   }
+// })
+
+
+
 document.getElementById('submit').onclick = function() {
   document.getElementById('output').innerHTML = '';
+  
+  var charCount = 0;
+  var outputStr = '';
 
   if (totalTweets === 0) {
     var audio = new Audio('23.mp3');
     audio.play();
   }
+
+
+  // Turn whole text value into a word array
+  var rawTextArr = textArea.value.split(' ');
+  var currTweetNo = 1;
+
+  // Loop through word array
+  rawTextArr.forEach(function(word, i){
+    var wordLength = word.length + 1;
+    charCount += wordLength;
+    var tweetCounter = `... (${currTweetNo} of ${totalTweets})`;
+    var currCharToRemove = tweetCounter.length;
+    
+    // If we're under 280 and it isn't the last word just add it to the current tweet
+    if (charCount + currCharToRemove <= 280 && i != rawTextArr.length - 1) {
+      outputStr += word + ' ';
+    }
+    // If we're under 280 and it is the last word add it to the current tweet and print
+    else if (charCount + currCharToRemove <= 280 && i == rawTextArr.length - 1) {
+      outputStr += word;
+      var finalOutput = `<li class="tweet"><span id="copy-confirm">copied!</span><p>${outputStr}${tweetCounter}</p><button onclick="copyTweet(${currTweetNo - 1})">copy tweet</button></li>`;
+      document.getElementById('output').innerHTML += finalOutput;
+    }
+    else if (charCount + charToRemove > 280) {
+      var finalOutput = `<li class="tweet"><span id="copy-confirm">copied!</span><p>${outputStr}${tweetCounter}</p><button onclick="copyTweet(${currTweetNo - 1})">copy tweet</button></li>`;
+      document.getElementById('output').innerHTML += finalOutput;
+      charCount = 0;
+      currTweetNo = currTweetNo + 1;
+      outputStr = word + ' ';
+    }
+  });
   
-  for (var i = 0; i < totalTweets; i++) {
-    var outputItems = document.getElementsByTagName('p');
-    var currCharToRemove = charToRemove;
+  // for (var i = 0; i < totalTweets; i++) {
+  //   var outputItems = document.getElementsByTagName('p');
+  //   var currCharToRemove = charToRemove;
 
-    if (i + 1 < 10) {
-      currCharToRemove = charToRemove;
-    }
-    else {
-      currCharToRemove = charToRemove + 1;
-    }
-    var charNeeded = 280 - currCharToRemove;
-    var rawOutput = textArea.value.substr(i*charNeeded,charNeeded);
-    var finalOutput = `<li class="tweet"><span id="copy-confirm">copied!</span><p>${rawOutput} (${i + 1} of ${totalTweets})</p><button onclick="copyTweet(${i})">copy tweet</button></li>`;
+  //   if (i + 1 < 10) {
+  //     currCharToRemove = charToRemove;
+  //   }
+  //   else {
+  //     currCharToRemove = charToRemove + 1;
+  //   }
+  //   var charNeeded = 280 - currCharToRemove;
+  //   var rawOutput = textArea.value.substr(i*charNeeded,charNeeded);
+  //   var finalOutput = `<li class="tweet"><span id="copy-confirm">copied!</span><p>${rawOutput} (${i + 1} of ${totalTweets})</p><button onclick="copyTweet(${i})">copy tweet</button></li>`;
 
-    document.getElementById('output').innerHTML += finalOutput;
-  }
+  //   document.getElementById('output').innerHTML += finalOutput;
+  // }
 };
 
 copyTweet = function(index) {
@@ -67,16 +124,5 @@ copyTweet = function(index) {
 }
 
 
-var testStr = 'I have a dream that one day this nation will rise up and live out the true meaning of its creed “We hold these truths to be self-evident, that all men are created equal.”';
+var testStr = 'An old man lived in the village. He was one of the most unfortunate people in the world. The whole village was tired of him; he was always gloomy, he constantly complained and was always in a bad mood. The longer he lived, the more bile he was becoming and the more poisonous were his words. People avoided him, because his misfortune became contagious. It was even unnatural and insulting to be happy next to him. He created the feeling of unhappiness in others. But one day, when he turned eighty years old, an incredible thing happened. Instantly everyone started hearing the rumour: “An Old Man is happy today, he doesn’t complain about anything, smiles, and even his face is freshened up.” The whole village gathered together. The old man was asked: Villager: What happened to you? “Nothing special. Eighty years I’ve been chasing happiness, and it was useless. And then I decided to live without happiness and just enjoy life. That’s why I’m happy now.” – An Old Man Moral of the story: Don’t chase happiness. Enjoy your life.';
 
-var modStr = testStr.split(' ');
-console.log(modStr);
-
-testCharCount = 0;
-
-modStr.forEach(function(word){
-  var wordLength = word.length;
-  testCharCount += wordLength;
-})
-
-console.log(testCharCount);
